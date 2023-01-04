5b2dfcc33eb4cd6cac87994e4b04b588e61f28a3
PTK <ptk@example.com>
2023-01-04 15:45:30 +0100

    Initial commit

---
+++ android/app/src/main/java/com/ptk/wanna_chat/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ptk.wanna_chat;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
